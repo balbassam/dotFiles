c714b8df1c6025d5757de26f6f1a49a3dafb43a6
GH0S1R33P0R <GH0S1R33P0R@git>
2012-09-19 04:58:54 -0400

    added runonce

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -379,7 +379,16 @@ client.add_signal("focus", function(c) c.border_color = beautiful.border_focus e
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
--- Auto start applications
-os.execute"dex -a -e Awesome"
+--  -- Auto start applications
+function run_once(cmd)
+  findme = cmd
+  firstspace = cmd:find(" ")
+  if firstspace then
+    findme = cmd:sub(0, firstspace-1)
+  end
+  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+end
 
-
+run_once("nm-applet")
+run_once("dropboxd")
+run_once("xfce4-power-manager")
